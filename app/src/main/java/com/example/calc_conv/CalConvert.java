ce1cd3de134f8cd0298d6d87674150644bce3ea3
Alyhermon <110520524+Alyhermon@users.noreply.github.com>
2022-09-22 11:36:54 -1200

    Initial commit

---
+++ app/src/main/java/com/example/calc_conv/CalConvert.java
@@ -0,0 +1,13 @@
+package com.example.calc_conv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalConvert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cal_convert);
+    }
+}
---
+++ app/src/main/java/com/example/calc_conv/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.calc_conv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtnombre;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtnombre = (EditText) findViewById(R.id.edtnombre);
+    }
+    public void siguiente(View V){
+        if (validar()){
+            Toast.makeText(this, "Entrando...", Toast.LENGTH_SHORT).show();
+            Intent i = new Intent(this, CalConvert.class);
+            startActivity(i);
+        }
+
+    }
+
+    public boolean validar(){
+
+        boolean retornar = true;
+        String edt1 = edtnombre.getText().toString();
+        if (edt1.isEmpty()){
+            edtnombre.setError("Este campo esta vacio, Escriba su Nombre");
+            retornar = false;
+        }
+        return retornar;
+    }
+}
